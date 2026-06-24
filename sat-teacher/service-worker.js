25fb999ebc3761b72190cdf1cacfc3e934a0a35c
Grace <grace@Graces-MacBook-Air-360.local>
2026-06-24 15:00:16 +0800

    Clarify SAT teacher gut expression

--- sat-teacher/service-worker.js
+++ sat-teacher/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sat-teacher-language-pwa-v12';
+const CACHE_NAME = 'sat-teacher-language-pwa-v13';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
