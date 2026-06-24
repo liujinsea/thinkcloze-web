b39b2b55a346f1d5709260810da6c83cfb167261
Grace <grace@Graces-MacBook-Air-360.local>
2026-06-24 15:19:46 +0800

    Remove skipped vocab terms and make match untimed

--- sat-words/SAT2020605A/service-worker.js
+++ sat-words/SAT2020605A/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sat-words-2020605a-pwa-v41';
+const CACHE_NAME = 'sat-words-2020605a-pwa-v43';
 const ASSETS = ['./','./index.html','./manifest.json','./barron3500.js','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
